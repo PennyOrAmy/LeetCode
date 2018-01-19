2e1928ea07d79f6458256486dbf60252742d1c5f
PennyOrAmy <penny_kay@126.com>
2018-01-20 00:04:19 +0800

    Best Time to Buy and Sell Stock II

---
+++ EasyCollection/Array/BestTimetoBuyandSellStockII.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  var length = prices.length;
+  var lastValue = length > 0 ? prices[0] : 0;
+  var delta = 0;
+  var sum = 0;
+
+  for (var i = 0; i < length; i++) {
+    delta = prices[i] - lastValue;
+    if (delta > 0) {
+      sum += delta;
+    }
+    lastValue = prices[i];
+  }
+
+  return length > 0 ? sum : 0;
+};
