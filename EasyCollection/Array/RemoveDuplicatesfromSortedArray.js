265bf29072048da6338db546729bfc071a66bcc3
PennyOrAmy <penny_kay@126.com>
2018-01-20 00:05:47 +0800

    Format

--- EasyCollection/Array/RemoveDuplicatesfromSortedArray.js
+++ EasyCollection/Array/RemoveDuplicatesfromSortedArray.js
@@ -3,24 +3,24 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    var counter = 0,
+  var counter = 0,
     lastValue,
-        numsLength = nums.length;
-    for(var i = 0; i < numsLength; i ++) {
-        if(nums[i] === lastValue) {
-            delete nums[i];
-            counter ++;
-        } else {
-          lastValue = nums[i];
-        }
+    numsLength = nums.length;
+  for (var i = 0; i < numsLength; i++) {
+    if (nums[i] === lastValue) {
+      delete nums[i];
+      counter++;
+    } else {
+      lastValue = nums[i];
     }
+  }
 
-    nums.sort(function(a,b) {
-      return a - b;
-    });
-    nums.splice(numsLength - counter, counter);
+  nums.sort(function(a, b) {
+    return a - b;
+  });
+  nums.splice(numsLength - counter, counter);
 
-    return nums.length;
+  return nums.length;
 };
 
 // Runtime: 163 ms
