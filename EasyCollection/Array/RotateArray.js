4739fb887c97150050fabe3ecbdc57e188fa40ab
PennyOrAmy <penny_kay@126.com>
2018-01-20 23:41:25 +0800

    Rotate Array

---
+++ EasyCollection/Array/RotateArray.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  var length = nums.length;
+  var moveOne;
+  k = k % length;
+  if (k != 0) {
+    if (k < length / 2) {
+      for (var i = 0; i < k; i++) {
+        moveOne = nums[length - 1];
+        nums.pop();
+        nums.unshift(moveOne);
+      }
+    } else {
+      for (var i = 0; i < length - k; i++) {
+        moveOne = nums[0];
+        nums.shift();
+        nums.push(moveOne);
+      }
+    }
+  }
+};
+// Runtime: 109 ms
+// Beats: 76.58%
