34acc7208663eb150bd0a89ba580ea6730b972ea
PennyOrAmy <penny_kay@126.com>
2018-01-30 11:49:57 +0800

    Valid Sudoku

--- EasyCollection/Array/TwoSum.js
+++ EasyCollection/Array/TwoSum.js
@@ -4,24 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let length = nums.length,
-        result = [],
-        isBreak = false;
+  let length = nums.length,
+    result = [];
 
-    for(let i = 0; i < length; i ++){
-        for(let j = i + 1; j< length; j ++) {
-            if(nums[i] + nums[j] === target) {
-                result = [i, j];
-                isBreak = true;
-                break;
-            }
-        }
-        if(isBreak) {
-            break;
-        }
+  for (let i = 0; i < length; i++) {
+    for (let j = i + 1; j < length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
+  }
 
-    return result;
-    // Runtime: 191 ms
-    // Beats: 55.93%
+  return result;
+  // Runtime: 191 ms
+  // Beats: 55.93%
 };
---
+++ EasyCollection/Array/ValidSudoku.js
@@ -0,0 +1,65 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+  // Each row must have the numbers 1-9 occuring just once.
+  for (let i = 0; i < 9; i++) {
+    let result = containsDuplicate(board[i]);
+    if (result) {
+      return false;
+    }
+  }
+
+  // Each column must have the numbers 1-9 occuring just once.
+  for (let i = 0; i < 9; i++) {
+    let array = [];
+    for (let j = 0; j < 9; j++) {
+      array.push(board[j][i]);
+    }
+    let result = containsDuplicate(array);
+    if (result) {
+      return false;
+    }
+  }
+
+  // And the numbers 1-9 must occur just once in each of the 9 sub-boxes of the grid.
+  for (let h = 0; h < 9;) {
+    for (let v = 0; v < 9;) {
+      let array = [];
+      for (let i = h; i < h + 3; i++) {
+        for (let j = v; j < v + 3; j++) {
+          array.push(board[i][j]);
+        }
+      }
+      let result = containsDuplicate(array);
+      if (result) {
+        return false;
+      }
+      v = v + 3;
+    }
+    h = h + 3;
+  }
+  return true;
+
+  // Runtime: 132 ms
+  // Beats: 73.58%
+};
+
+var containsDuplicate = function(nums) {
+  let result = false,
+    newNums = [],
+    value;
+  for (let i = 0; i < nums.length; i++) {
+    value = nums[i];
+    if (value === '.') {
+      continue;
+    } else if (typeof(newNums[value]) !== 'undefined') {
+      result = true;
+      break;
+    } else {
+      newNums[value] = value;
+    }
+  }
+  return result;
+};
