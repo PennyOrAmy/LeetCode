8a580ba632e2e01f298b7686ec3502368d880673
PennyOrAmy <penny_kay@126.com>
2018-02-06 21:45:40 +0800

    First Unique Character in a String

---
+++ EasyCollection/String/FirstUniqueCharacterInAString.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+  let match = [],
+    length = s.length;
+  for (let i = 0; i < length; i++) {
+    let key = s.charCodeAt(i) - 97,
+      item = match[key];
+    if (typeof(item) === 'undefined') {
+      match[key] = 1;
+    } else {
+      match[key] = item + 1;
+    }
+  }
+
+  for (let i = 0; i < length; i++) {
+    let key = s.charCodeAt(i) - 97,
+      item = match[key];
+    if (item === 1) {
+      return i;
+    }
+  }
+
+  return -1;
+};
