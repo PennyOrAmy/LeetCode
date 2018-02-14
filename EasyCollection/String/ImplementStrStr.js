9c6d445d8daa335179489b5e1ea0ff947d41b974
PennyOrAmy <penny_kay@126.com>
2018-02-14 15:24:49 +0800

    Implement strStr

---
+++ EasyCollection/String/ImplementStrStr.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  let hLength = haystack.length,
+    nLength = needle.length;
+  for (var i = 0;; i++) {
+    for (var j = 0;; j++) {
+      if (j === nLength) {
+        return i;
+      }
+      if (i + j === hLength) {
+        return -1;
+      }
+      if (haystack.charAt(i + j) !== needle.charAt(j)) {
+        break;
+      }
+    }
+  }
+};
