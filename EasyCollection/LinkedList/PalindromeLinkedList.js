713de76f208310e5ba5e39e411b7996bee382451
PennyOrAmy <penny_kay@126.com>
2018-03-01 23:31:31 +0800

    Palindrome Linked List

---
+++ EasyCollection/LinkedList/PalindromeLinkedList.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+  let i = 0,
+    temp = head,
+    vals = [];
+  while (temp !== null) {
+    vals.push(temp.val);
+    temp = temp.next;
+    i++;
+  }
+
+  i = 0;
+  let length = vals.length;
+  while (i < length) {
+    if (vals[i] !== vals[length - i - 1]) {
+      return false;
+    }
+    i++;
+  }
+  return true;
+};
