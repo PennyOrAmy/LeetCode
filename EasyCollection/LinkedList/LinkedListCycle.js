b10f43a2495e606149347a056f3ecd5f021c4517
PennyOrAmy <penny_kay@126.com>
2018-03-03 23:21:05 +0800

    Linked List Cycle

---
+++ EasyCollection/LinkedList/LinkedListCycle.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  let fast = head,
+    slow = head;
+  while (true) {
+    if (fast === null || slow === null) {
+      return false;
+    }
+    if (fast.next !== null) {
+      fast = fast.next.next;
+    } else {
+      return false;
+    }
+    slow = slow.next;
+
+    if (fast === slow) {
+      return true;
+    }
+  }
+};
