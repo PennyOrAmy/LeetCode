d97ceecc76634a20c3f61900a4378e9f574b702b
PennyOrAmy <penny_kay@126.com>
2018-03-09 23:58:30 +0800

    Maximum Depth of Binary Tree

---
+++ Trees/MaximumDepthofBinaryTree.js
@@ -0,0 +1,10 @@
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (root === null) {
+    return 0;
+  }
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
