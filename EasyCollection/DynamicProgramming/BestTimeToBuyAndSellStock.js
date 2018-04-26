ff2c543d9e8a716d47d4a53ad7ca67038d13305d
PennyOrAmy <penny_kay@126.com>
2018-04-26 13:18:18 +0800

    Best Time to Buy and Sell Stock

---
+++ EasyCollection/DynamicProgramming/BestTimeToBuyAndSellStock.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+	let i = -1,
+		l = prices.length,
+		k = prices[0],
+		max = 0;
+	while (i++ < l) {
+		if (i + 1 < l) {
+			k = Math.min(k, prices[i]);
+			max = Math.max(max, prices[i + 1] - k);
+		}
+	}
+	return max;
+};
