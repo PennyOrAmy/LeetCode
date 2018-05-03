b310972809f5d70fe14be3d37ee7cbe287fb8147
PennyOrAmy <penny_kay@126.com>
2018-05-03 21:06:28 +0800

    House Robber

---
+++ EasyCollection/DynamicProgramming/HouseRobber.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+	let a = 0,
+		b = 0,
+		isOdd = true;
+	nums.forEach((i) => {
+		if (isOdd) {
+			a = Math.max(a + i, b);
+		} else {
+			b = Math.max(a, b + i);
+		}
+		isOdd = !isOdd;
+	});
+	return Math.max(a, b);
+};
