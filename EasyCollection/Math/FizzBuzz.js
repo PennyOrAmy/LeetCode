1777171f7a0defc953e1aea263fafe64955f3bdb
PennyOrAmy <penny_kay@126.com>
2018-05-09 10:42:24 +0800

    fizz buzz

---
+++ EasyCollection/Math/FizzBuzz.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+	let r = [],
+		i = 0;
+	while (i++ < n) {
+		let t = '';
+		if (i % 3 === 0) {
+			t += 'Fizz';
+		}
+		if (i % 5 === 0) {
+			t += 'Buzz';
+		}
+		if (t === '') {
+			t += i;
+		}
+		r.push(t);
+	};
+	return r;
+};
