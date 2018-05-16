3337b0905856be13959007bdfc8676ac1b6bcdb8
PennyOrAmy <penny_kay@126.com>
2018-05-16 12:08:52 +0800

    Power of Three

---
+++ EasyCollection/Math/PowerOfThree.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+	let max = getMaxPowerOfThree();
+	return n > 0 && max % n === 0;
+};
+
+var getMaxPowerOfThree = function() {
+	let item = 1;
+	while (item < Number.MAX_SAFE_INTEGER - 1) {
+		item *= 3;
+	}
+	return item / 3;
+}
