4ef2ba98dc9e4b5283bc70ec0be1257674a3dc89
PennyOrAmy <penny_kay@126.com>
2018-05-22 23:24:52 +0800

    Roman to Integer

---
+++ EasyCollection/Math/RomanToInteger.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+let map = {
+	default: 0,
+	I: 1,
+	V: 5,
+	X: 10,
+	L: 50,
+	C: 100,
+	D: 500,
+	M: 1000
+}
+var romanToInt = function(s) {
+	let r = 0,
+		lk = 'default';
+	for (let i = s.length - 1; i >= 0; i--) {
+		let k = s[i];
+		let v = map[k];
+		if (v >= map[lk]) {
+			r += v;
+		} else {
+			r -= v;
+		}
+		lk = k;
+	}
+	return r;
+};
